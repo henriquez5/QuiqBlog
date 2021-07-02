b244fbd6d9d64034a8685c90cf8cb8ec8a6cc482
Henrique Jorge A Craveiro <henrique.craveiro@fitbank.com.br>
2021-07-02 13:54:48 -0300

    Adicionar arquivos de projeto.

---
+++ QuiqBlog/Configuration/AppConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuiqBlog.Configuration
+{
+    public static class AppConfiguration
+    {
+        public static void AddDefaultConfiguration( this IApplicationBuilder aplicationBuilder, IWebHostEnvironment webHostEnviromment)
+        {
+            if (webHostEnviromment.IsDevelopment())
+            {
+                aplicationBuilder.UseDeveloperExceptionPage();
+                aplicationBuilder.UseDatabaseErrorPage();
+            }
+            else
+            {
+                aplicationBuilder.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                aplicationBuilder.UseHsts();
+            }
+            aplicationBuilder.UseHttpsRedirection();
+            aplicationBuilder.UseStaticFiles();
+
+            aplicationBuilder.UseRouting();
+
+            aplicationBuilder.UseAuthentication();
+            aplicationBuilder.UseAuthorization();
+
+            aplicationBuilder.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
---
+++ QuiqBlog/Configuration/AppServices.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QuiqBlog.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuiqBlog.Configuration
+{
+    public static class AppServices
+    {
+        public static void AddDefaultServices(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+            serviceCollection.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            serviceCollection.AddControllersWithViews();
+            serviceCollection.AddRazorPages();
+        }
+    }
+}
